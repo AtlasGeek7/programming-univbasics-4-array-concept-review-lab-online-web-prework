1b00fd0997429b4148f75bd50f5f2ba5770f01fe
Khalid Tissire <ghettoguy7@gmail.com>
2019-12-08 07:13:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,8 +9,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort
-  return array[-1]
+  sorted_array = array.sort
+  return sorted_array[-1]
 end
 
 def find_min_value(array)
